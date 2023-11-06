18b7516b3c6f9eddc50ed978b77abb0fdfc9ebbe
Carlo <carlo_del_piero@hotmail.com>
2023-11-06 15:38:12 +0100

    Commento di come ho spostato e ripulito le commit di master

--- src/pages/pageBellido.tsx
+++ src/pages/pageBellido.tsx
@@ -3,6 +3,7 @@ import Logo from "../components/bellido/logo";
 const PageBellido = () => {
     return <div><h1>Carlo Bellido </h1>
     <Logo/>
+    <p>Ho fatto tutte le modifiche su master, poi un merge di master nel branch in Bellido-init, poi hard reset in master tornando alla situazione prima delle modifiche</p>
     </div>;
   };
   
