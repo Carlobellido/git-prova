d468abbd7ffb3dc295d4eca91b9c1f20da8f4112
Carlo <carlo_del_piero@hotmail.com>
2023-11-06 15:02:40 +0100

    aggiunta logo in page

--- src/pages/pageBellido.tsx
+++ src/pages/pageBellido.tsx
@@ -1,5 +1,9 @@
+import Logo from "../components/bellido/logo";
+
 const PageBellido = () => {
-    return <div><h1>Carlo Bellido </h1></div>;
+    return <div><h1>Carlo Bellido </h1>
+    <Logo/>
+    </div>;
   };
   
   export default PageBellido;
